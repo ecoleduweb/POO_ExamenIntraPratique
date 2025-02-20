1461e7682b06d91a4797bf045293694ea2b4e4ab
antoine chagnon michaud <antoine.chagnonmichaud@cegeprdl.ca>
2025-02-20 14:20:40 -0500

    removed the code

--- Pratique/MainWindow.xaml.cs
+++ Pratique/MainWindow.xaml.cs
@@ -14,24 +14,30 @@ namespace Pratique
 
         private void OnTerrainSansServiceClick(object sender, RoutedEventArgs e)
         {
-            Service[] services = GetServicesFromWpfInterface();
-            var terrainSauvage = new TerrainSauvage(services);
-            TbCoutEstime.Text = terrainSauvage.CalculerCout().ToString("0.00$");
+            // La variable services est un tableau de deux services. Simplement enlever les commentaires pour que le code fonctionne.
+            //Service[] services = GetServicesFromWpfInterface();
+
+            // crée l'objet terrainSauvage.
+            // modifie la valeur du TextBock à partir du résultat de la méthode CalculerCout() de terrainSauvage.
         }
 
         public void OnTerrainAvecServicesClick(object sender, RoutedEventArgs e)
         {
-            Service[] services = GetServicesFromWpfInterface();
-            var terrainAvecServices = new TerrainAvecServices(services);
-            TbCoutEstime.Text = terrainAvecServices.CalculerCout().ToString("0.00$");
+            // La variable services est un tableau de deux services. Simplement enlever les commentaires pour que le code fonctionne.
+            //Service[] services = GetServicesFromWpfInterface();
+
+            // crée l'objet terrainAvecServices.
+            // modifie la valeur du TextBock à partir du résultat de la méthode CalculerCout() de terrainAvecServices.
         }
 
-        private Service[] GetServicesFromWpfInterface()
-        {
-            return [
-                new Service(10, CkBxAvec20A.IsChecked.Value),
-                new Service(15, CkBxAvecEgouts.IsChecked.Value)
-            ];
-        }
+        // J'ai mis la métode ici en commentaire. En ajoutant ta classe Service, le code que j'ai ici ne devrait plus être en erreur.
+        // Assures-toi de mettre la coût et ensuite le boolean dans le constructeur de Service.
+        //private Service[] GetServicesFromWpfInterface()
+        //{
+        //    return [
+        //        new Service(10, CkBxAvec20A.IsChecked.Value),
+        //        new Service(15, CkBxAvecEgouts.IsChecked.Value)
+        //    ];
+        //}
     }
 }
